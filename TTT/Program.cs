f35154a87580cc1c1e875c00384e9d9ce508bf07
BlackHatBadshah <sanjayv@appinfoinc.com>
2024-04-18 03:01:17 +0530

    Renamed Library

--- TTT/Program.cs
+++ TTT/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace TTT
+namespace TicTacToe
 {
     public static class Program
     {
@@ -8,14 +8,14 @@ namespace TTT
         {
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
             Console.WriteLine("enter 1 for dumb bot else continue");
-           var data =  Console.ReadLine();
+            var data = Console.ReadLine();
             // Creating a TicTacToe game with one human player
             Models.Player humanPlayer = new Models.Player('X', Models.PlayerType.Human);
-            Models.Player botPlayer = new Models.Player('O', data=="1"?Models.PlayerType.RandomBot:Models.PlayerType.SmartBot);
-            Models.TicTacToe game = new Models.TicTacToe(humanPlayer, botPlayer, 3,3);
+            Models.Player botPlayer = new Models.Player('O', data == "1" ? Models.PlayerType.RandomBot : Models.PlayerType.SmartBot);
+            Models.TicTacToe game = new Models.TicTacToe(humanPlayer, botPlayer, 3, 3);
 
             // Start the game
-         
+
             Console.WriteLine("You are playing as X. Make your move by entering the column and row numbers.");
 
             game.Play();
